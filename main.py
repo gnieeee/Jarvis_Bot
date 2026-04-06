528d23ef742db5ca26819e8ceb805f4db21d6e2d
Ignazio Provenzano <ignazioprovenzano006@gmail.com>
2026-04-06 19:06:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask, request
 
 # --- SISTEMI CORE ---
 TOKEN = os.getenv("TELEGRAM_TOKEN")
-genai.configure(api_key="AIzaSyBigQ2m9Oo7M8n5GVVBErxO-gn2y0OQObM")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 model = genai.GenerativeModel('gemini-1.0-flash')
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
